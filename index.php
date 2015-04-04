5127f08c2c082969275101d9c8839bd331860acb
LeonelCabrera <jose.cabrera17@hotmail.com>
2015-04-04 01:06:46 -0600

    validacion del loggeo

--- index.php
+++ index.php
@@ -30,13 +30,13 @@
 
 
 
-        <form action="home.php" method="post">
+        <form action="validar.php" method="post">
           <div class="form-group has-feedback">
-            <input type="text" class="form-control" placeholder="Carnet"/>
+            <input type="text" name="carnet" class="form-control" placeholder="Carnet/NIT"/>
             <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
           </div>
           <div class="form-group has-feedback">
-            <input type="password" class="form-control" placeholder="Contraseña"/>
+            <input type="password" name="pass" class="form-control" placeholder="Contraseña"/>
             <span class="glyphicon glyphicon-lock form-control-feedback"></span>
           </div>
           <div class="row">
